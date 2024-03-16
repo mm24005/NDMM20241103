c85ddd9dbd3c62300696ceb7363e69b064cf293c
Nery David Méndez Méndez <mm24005@esfe.agape.edu.sv>
2024-03-16 10:58:52 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NDMM20241103.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
