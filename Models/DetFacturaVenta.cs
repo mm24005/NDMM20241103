a8e6c1aa5319bbce44f1a4b469ed073689445142
Nery David Méndez Méndez <mm24005@esfe.agape.edu.sv>
2024-03-17 13:29:01 -0600

    Data Annotations

--- Models/DetFacturaVenta.cs
+++ Models/DetFacturaVenta.cs
@@ -1,11 +1,21 @@
-﻿namespace NDMM20241103.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace NDMM20241103.Models
 {
     public class DetFacturaVenta
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "La fecha es obligatoria")]
         public int IdFacturaVenta { get; set; }
+
+        [Required(ErrorMessage = "Digite el nombre del producto")]
         public string Producto { get; set; } = null!;
+
+        [Required(ErrorMessage = "Digite la cantidad")]
         public int Cantidad { get; set; }
+
+        [Required(ErrorMessage = "Digite el precio unitario")]
         public decimal PrecioUnitario { get; set; }
 
         public virtual FacturaVenta IdFacturaVentaNavigation { get; set; } = null!;
--- Models/FacturaVenta.cs
+++ Models/FacturaVenta.cs
@@ -1,4 +1,6 @@
-﻿namespace NDMM20241103.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace NDMM20241103.Models
 {
     public class FacturaVenta
     {
@@ -9,7 +11,11 @@
 
         public int Id { get; set; }
         public DateTime FechaVenta { get; set; }
+
+        [Required(ErrorMessage = "Digite el correlativo")]
         public string Correlativo { get; set; } = null!;
+
+        [Required(ErrorMessage = "Digite el nombre del cliente")]
         public string Cliente { get; set; } = null!;
         public decimal TotalVenta { get; set; }
 
