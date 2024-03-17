fbd8a660d0e86f916de5c16f9ff4ef6c42afc1a6
Nery David Méndez Méndez <mm24005@esfe.agape.edu.sv>
2024-03-17 08:28:30 -0600

    Migraciones

---
+++ Migrations/20240317140527_InitialCreate.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NDMM20241103.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FacturaVentas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaVenta = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Correlativo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cliente = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TotalVenta = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FacturaVentas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DetFacturaVentas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdFacturaVenta = table.Column<int>(type: "int", nullable: false),
+                    Producto = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    PrecioUnitario = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    IdFacturaVentaNavigationId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DetFacturaVentas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DetFacturaVentas_FacturaVentas_IdFacturaVentaNavigationId",
+                        column: x => x.IdFacturaVentaNavigationId,
+                        principalTable: "FacturaVentas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetFacturaVentas_IdFacturaVentaNavigationId",
+                table: "DetFacturaVentas",
+                column: "IdFacturaVentaNavigationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DetFacturaVentas");
+
+            migrationBuilder.DropTable(
+                name: "FacturaVentas");
+        }
+    }
+}
